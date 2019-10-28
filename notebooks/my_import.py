0a4edea37ecc245a9021faa1a004425a795d9a37
Alessio Margan <alessio.margan@iit.it>
2019-10-28 16:33:42 +0100

    just notebooks ....

---
+++ notebooks/my_import.py
@@ -0,0 +1,31 @@
+import socket
+
+import ipywidgets.widgets as widgets
+from IPython.display import display
+
+from ecat_repl import ZmsgIO
+from ecat_repl import CtrlCmd
+from ecat_repl import SdoCmd
+from ecat_repl import (
+    master_cmd_stop,
+    master_cmd_start,
+    master_cmd_get_slave_descr,
+    #
+    flash_cmd_load_default,
+    flash_cmd_save_flash,
+    flash_cmd_load_default,
+    #
+    ctrl_cmd_start,
+    ctrl_cmd_stop,
+    ctrl_cmd_fan,
+    ctrl_cmd_led,
+    ctrl_cmd_run_torque_calib,
+    ctrl_cmd_set_zero,
+    ctrl_cmd_set_min_pos,
+    ctrl_cmd_set_max_pos,
+    #
+    ctrl_cmd_dac_tune,
+    #
+    ctrl_cmd_test_done,
+    ctrl_cmd_test_error,
+)
